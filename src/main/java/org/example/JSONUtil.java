c9dcf65b71891725310449fed34e21eb23e5d81e
leopold1511 <bavrinleonid@gmail.com>
2024-06-26 12:49:57 +0300

    Refactor and customize styles of graph

--- src/main/java/org/example/JSONUtil.java
+++ src/main/java/org/example/JSONUtil.java
@@ -1,40 +1,35 @@
 package org.example;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import datamodel.Product;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
+
 
 public class JSONUtil {
 
-    public static ArrayList<Product> importFromJson(String filePath) throws IOException {
+    public static Product importFromJson(String filePath) throws IOException {
         Gson gson = new Gson();
         FileReader reader = new FileReader(filePath);
 
-        // Read the entire JSON object
         ProductData productData = gson.fromJson(reader, ProductData.class);
 
-        // Close the reader
         reader.close();
 
-        // Return the list of products
-        return productData.getProducts();
+        return productData.getProduct();
     }
 
     static class ProductData {
-        private ArrayList<Product> products;
+        private Product product;
 
-        public ArrayList<Product> getProducts() {
-            return products;
+        public Product getProduct() {
+            return product;
         }
 
-        public void setProducts(ArrayList<Product> products) {
-            this.products = products;
+        public void setProducts(Product product) {
+            this.product = product;
         }
     }
 }
